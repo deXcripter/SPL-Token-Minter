84f86ee2bd7a0759ed52f4fc45bd00f8f2005baa
johnpaul <dexcripter@gmail.com>
2025-02-15 00:52:40 +0100

    receiving sol drop

--- class-2/app/page.tsx
+++ class-2/app/page.tsx
@@ -7,10 +7,12 @@ import {
 } from "@solana/wallet-adapter-react-ui";
 import { Connection } from "@solana/web3.js";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 function Home() {
   const { publicKey, sendTransaction } = useWallet();
   const [solBalance, setSolBalance] = useState<number | null>(0);
+  const [solBalanceLoading, setSolBalanceLoading] = useState(false);
 
   const connection = new Connection("https://api.devnet.solana.com");
   useEffect(() => {
@@ -20,8 +22,31 @@ function Home() {
   const setBalance = async () => {
     if (!publicKey) return;
     const conn = await connection.getBalance(publicKey);
-    console.log(conn);
-    setSolBalance(conn / 1000000000); // solana has 9 decimal places
+    setSolBalance(conn / 1000000000);
+  };
+
+  const recieveSol = async () => {
+    setSolBalanceLoading(true);
+    try {
+      if (!publicKey) return;
+      const signature = await connection.requestAirdrop(publicKey, 1000000000);
+
+      const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash();
+
+      await connection.confirmTransaction({
+        blockhash,
+        lastValidBlockHeight,
+        signature,
+      });
+
+      setBalance();
+    } catch (err: any) {
+      toast.error(err.message);
+    }
+    {
+      setSolBalanceLoading(false);
+    }
   };
 
   return (
@@ -29,15 +54,24 @@ function Home() {
       <section className="w-[40%] h-[50%] bg-blue-200 py-5 rounded-md text-slate-900">
         <div className="flex justify-between mx-10">
           <WalletMultiButton />
-          <WalletDisconnectButton onClick={() => setSolBalance(null)} />
+          <WalletDisconnectButton />
         </div>
 
-        <div>
-          <div className="flex p-10">
-            <h1 className="text-2xl">
-              Wallet Balance: {solBalance?.toString() || "Connect Wallet"}
-            </h1>
-          </div>
+        <div className="flex p-10">
+          <h1 className="text-2xl">
+            Wallet Balance:
+            {typeof solBalance === "number"
+              ? solBalance?.toString() + " sol"
+              : "Wallet not Connected"}
+          </h1>
+        </div>
+        <div className="px-10">
+          <button
+            onClick={recieveSol}
+            className="bg-red-300 px-5 py-2 rounded-md"
+          >
+            Recieve Airdrop
+          </button>
         </div>
       </section>
     </div>
